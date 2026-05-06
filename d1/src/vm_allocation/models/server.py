a2ee9195a3ff70341116c47da69f628984478438
DEMOKLYR <mustapha.oumeziane@epita.fr>
2026-05-06 19:50:33 +0200

    add serveur class

--- d1/src/vm_allocation/models/server.py
+++ d1/src/vm_allocation/models/server.py
@@ -1,2 +1,57 @@
 class Server:
-    pass
+    def __init__(self, server_id, cpu, ram, storage, bw):
+
+        self.id = server_id
+        self.cpu_capacity = cpu
+        self.ram_capacity = ram
+        self.storage_capacity = storage
+        self.bw_capacity = bw
+
+        self.cpu_usage = 0
+        self.ram_usage = 0
+        self.storage_usage = 0
+        self.bw_usage = 0
+
+        self.vms = []
+
+    def remove_vm_by_id(self, vm_id):
+        for vm in self.vms:
+            if vm.id == vm_id:
+                self.cpu_usage -= vm.cpu
+                self.ram_usage -= vm.ram
+                self.storage_usage -= vm.storage
+                self.bw_usage -= vm.bw
+
+                self.vms.remove(vm)
+
+                return True
+
+        return False
+
+
+    def add_vm(self, vm):
+
+        if not self.can_host(vm):
+            return False
+        self.vms.append(vm)
+
+        self.cpu_usage += vm.cpu
+        self.ram_usage += vm.ram
+        self.storage_usage += vm.storage
+        self.bw_usage += vm.bw
+
+        return True
+
+    def can_host(self, vm):
+
+        if (self.cpu_usage + vm.cpu > self.cpu_capacity or
+            self.ram_usage + vm.ram > self.ram_capacity or
+            self.storage_usage + vm.storage > self.storage_capacity or
+            self.bw_usage + vm.bw > self.bw_capacity):
+            return False
+
+        for other_vm in self.vms:
+            if other_vm.id in vm.anti_affinity:
+                return False
+
+        return True
