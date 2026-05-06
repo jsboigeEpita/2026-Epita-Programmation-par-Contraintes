4c5dc03219fa8877e7721584ad5c4d4e2a036bb1
coraline.carpin <coraline.carpin@epita.fr>
2026-05-06 19:49:31 +0200

    feat: vm class

--- d1/src/vm_allocation/models/vm.py
+++ d1/src/vm_allocation/models/vm.py
@@ -1,2 +1,29 @@
 class VM:
-    pass
+    def __init__(self, vm_id, cpu, ram, storage, bw):
+        self.id = vm_id
+
+        self.cpu = cpu
+        self.ram = ram
+        self.storage = storage
+        self.bw = bw
+        self.affinity = set()
+        self.anti_affinity = set()
+
+    def requirements(self) -> dict[str, int]:
+        return {
+            "cpu": self.cpu,
+            "ram": self.ram,
+            "storage": self.storage,
+            "bw": self.bw,
+        }
+
+    def total_requirement(self) -> int:
+        return sum(self.requirements().values())
+
+    def add_affinity(self, vm: "VM") -> None:
+        self.affinity.add(vm.id)
+        vm.affinity.add(self.id)
+
+    def add_anti_affinity(self, vm: "VM") -> None:
+        self.anti_affinity.add(vm.id)
+        vm.anti_affinity.add(self.id)
