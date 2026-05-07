e5d62e3dbefa02f53a35a4941c899054395cbea6
brendan.martin <brendan.martin@epita.fr>
2026-05-07 11:08:03 +0200

    doc(models): added documentation

--- d1/src/vm_allocation/models/__init__.py
+++ d1/src/vm_allocation/models/__init__.py
@@ -1,3 +1,15 @@
+"""Provides models used to abstract the VM allocation problem.
+
+======= =========================================================================
+Classes Description
+------- -------------------------------------------------------------------------
+Context Datacenter context, containing populated servers with various capacities.
+Server  Model representing a server with a capacity and running VMs.
+Solver  Interface providing the template for VM allocation problem solvers.
+VM      Model representing a VM with requirements.
+======= =========================================================================
+"""
+
 from .context import Context
 from .server import Server
 from .solver import Solver
--- d1/src/vm_allocation/models/context.py
+++ d1/src/vm_allocation/models/context.py
@@ -1,3 +1,5 @@
+"""Provides the Context class."""
+
 from __future__ import annotations
 
 from typing import List
@@ -7,6 +9,14 @@ from .server import Server
 
 
 class Context:
+    """Datacenter context, containing populated servers with various capacities.
+
+    Attributes
+    ----------
+    servers : List[Server] | None, optional
+        List of servers to populate the context with at setup, by default None.
+    """
+
     def __init__(self, servers: List[Server] | None = None):
         self.servers: dict[UUID, Server] = {}
         if servers is not None:
@@ -14,18 +24,68 @@ class Context:
                 self.add_server(server)
 
     def add_server(self, server: Server | None = None, **kwargs) -> UUID:
+        """Adds a server to the context by object or kwargs construction.
+
+        Parameters
+        ----------
+        server : Server | None, optional
+            The server to add, by default None.
+
+        Returns
+        -------
+        UUID
+            The generated id of the server if it was created by kwargs.
+        """
         new_server = server or Server(UUID(), **kwargs)
         self.servers[new_server.id] = new_server
         return new_server.id
 
     def remove_server(self, server_id: UUID):
+        """Removes a server from the context by id.
+
+        Parameters
+        ----------
+        server_id : UUID
+            The id of the server to remove.
+        """
         self.servers.pop(server_id)
 
     def get_server(self, server_id: UUID) -> Server:
+        """Gets a specific server object from the context.
+
+        Parameters
+        ----------
+        server_id : UUID
+            The id of the server to get.
+
+        Returns
+        -------
+        Server
+            The fetched server.
+
+        Raises
+        ------
+        KeyError
+            The server id didn't exist.
+        """
         return self.servers[server_id]
 
     def get_servers(self) -> List[Server]:
+        """Gets a list of all servers.
+
+        Returns
+        -------
+        List[Server]
+            The list of all the servers.
+        """
         return list(server for server in self.servers.values())
 
     def copy(self) -> Context:
+        """Creates a new context instance from this context.
+
+        Returns
+        -------
+        Context
+            The copied context.
+        """
         return Context([server.copy() for server in self.get_servers()])
